8e511a2d54df6a34dd37dbb13e79a492b6dd20d0
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-29 21:34:02 +0530

    Update start_command_text.py

--- plugins/start_command_text.py
+++ plugins/start_command_text.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters, StopPropagation
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import time
 bot_start_time = time.time()
-
+    
 ''' def get_readable_time(seconds: int) -> str:
     result = ''
     (days, remainder) = divmod(seconds, 86400)
@@ -24,7 +24,29 @@ bot_start_time = time.time()
 @Client.on_message(filters.command(["start"]), group=-2)
 async def start(client, message):
     bot_uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)) 
-
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤖 Update Channel', url='https://telegram.me/tellybots_4u'),
+        InlineKeyboardButton('💬 Support Group', url='https://telegram.me/tellybots_support')
+        ],[
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('👲 About', callback_data='about'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Multipurpose Bot that can perform many functions.\n\n/help for More info \n Bot Uptime : {bot_uptime}"
-    await message.reply_text(welcomed, reply_markup=joinButton)
+    await message.reply_text(welcomed, reply_markup=START_BUTTONS)
     raise StopPropagation
