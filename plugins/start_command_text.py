78e9aa8eefc74bd110c749b4ec3e3f2a8b8c0873
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-29 21:31:13 +0530

    Update start_command_text.py
        joinButton = InlineKeyboardMarkup([
            [InlineKeyboardButton("JOIN", url="https://t.me/TGBotsCollection")],
            [InlineKeyboardButton(
                "Try", url="https://t.me/TGBotsCollectionbot")]
        ])

--- plugins/start_command_text.py
+++ plugins/start_command_text.py
@@ -24,11 +24,7 @@ bot_start_time = time.time()
 @Client.on_message(filters.command(["start"]), group=-2)
 async def start(client, message):
     bot_uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)) 
-    joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("JOIN", url="https://t.me/TGBotsCollection")],
-        [InlineKeyboardButton(
-            "Try", url="https://t.me/TGBotsCollectionbot")]
-    ])
+
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Multipurpose Bot that can perform many functions.\n\n/help for More info \n Bot Uptime : {bot_uptime}"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
