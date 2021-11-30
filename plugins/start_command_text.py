0e101a00747c2609192a145f95d695270c1008af
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 06:42:32 +0530

    Update start_command_text.py

--- plugins/start_command_text.py
+++ plugins/start_command_text.py
@@ -21,18 +21,82 @@ bot_start_time = time.time()
     result += f'{seconds}s'
     return result '''
 
-@Client.on_message(filters.command(["start"]), group=-2)
-async def start(client, message):
-    bot_uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)) 
-    joinButton = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton("Update Channel", url="https://telegram.me/tellybots_4u"),
-        InlineKeyboardButton("Support Group", url="https://telegram.me/tellybots_support")
-        ],[
-        InlineKeyboardButton("Help", callback_data="help"),
-        InlineKeyboardButton("Close", callback_data="close")
-        ]]
-     welcomed = f"Script.START_TEXT"
-    await message.reply_text(welcomed, reply_markup=joinButton)
-    raise StopPropagation
 
+@Client.on_message(filters.command('start') & filters.private & ~filters.edited)
+async def start(b, m):
+    if not await db.is_user_exist(m.from_user.id):
+        await db.add_user(m.from_user.id)
+        await b.send_message(
+            Var.BIN_CHANNEL,
+            f"**Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ:** \n\nMy New Freind [{m.from_user.first_name}](tg://user?id={m.from_user.id}) Started Your Bot!!"
+        )
+    usr_cmd = m.text.split("_")[-1]
+    if usr_cmd == "/start":
+        if Var.UPDATES_CHANNEL != "None":
+            try:
+                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+                if user.status == "kicked":
+                    await b.send_message(
+                        chat_id=m.chat.id,
+                        text="Sorry Sir, Yᴏᴜ Are Banned To Use Me. Contact Our Support Group\n\n @tellybots_support **They Will help you**",
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="<b>Join My Update Channel To Use Me 🤖</b>",
+                    reply_markup=InlineKeyboardMarkup(
+                        [[
+                            InlineKeyboardButton("Join Now 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            ]]
+                    ),
+                    parse_mode="HTML"
+                )
+                return
+            except Exception:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="<b>Something Went Wrong Contact Our Support Group</i> <b><a href='http://t.me/tellybots_support'>[ Click Here ]</a></b>",
+                    parse_mode="HTML",
+                    disable_web_page_preview=True)
+                return
+        await m.reply_text(
+            text=START_TEXT.format(m.from_user.mention),
+            parse_mode="HTML",
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+              )                                                                         
+                                                                                       
+                                                                            
+    else:
+        if Var.UPDATES_CHANNEL != "None":
+            try:
+                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+                if user.status == "kicked":
+                    await b.send_message(
+                        chat_id=m.chat.id,
+                        text="**Sorry Sir, You Are Banned To Use Me. Contact Our Support Group** @tellybots_support",
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**Please Join My Update Channel To Use Me**!\n\n**Due To Overload, Only Channel Subscribers Can Use Me**!",
+                    reply_markup=InlineKeyboardMarkup(
+                        [[
+                          InlineKeyboardButton("🤖 Jᴏin Update Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            ]]
+                    ),
+                    parse_mode="markdown"
+                )
+                return
+            except Exception:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**Something Went Wrong. Contact Our Support Group** [Tellybots_Support](https://t.me/tellybots_support).",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True)
