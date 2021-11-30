2c4bedb4f3957a451494a7f9e09052212170c820
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 17:46:47 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,60 @@
+class Script(object):
+    START_TEXT = """<b>Hey! {}\n
+I'm Renamer + File To Video Converter Bot With Permanent Thumbnail Support!⚡</b>
+
+<b>Do /help for more Details ...</b>
+"""
+
+    RENAME_403_ERR = "What Are You Doing? Bruuh!!"
+    BANNED_USER_TEXT = "Sorry!! But Mr. Dlaize Banned You From Using This Service. That Means You Can't Run ME Now! \n \n Contact : @Dlaize For more Details.. " 
+    ABS_TEXT = "What Are You Trying To Do, Mate?"
+    UPGRADE_TEXT = "CONTACT : Mr. @Dlaize"
+    DOWNLOAD_START = "<b>╭─ ⌊ 📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ⌉ \n│\n</b>"
+    UPLOAD_START = "<b>╭─ ⌊ 📤 ᴜᴘʟᴏᴀᴅɪɴɢ ⌉ \n│\n</b>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me SHARE > ⚡</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT : <a href='https://telegram.me/dlaize'> ᴍʀ. ᴅʟᴀɪᴢᴇ</a>"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact Mr. @Dlaize"
+    SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Saved ✅ This Is Permanent Until</b> ❌ /delthumb ❌"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Cleared Succesfully🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media Cleared Succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename file name extension To rename a file</b>"
+    REPLY_TO_FILE_FOR_CONVERT = "<b> Please Reply To An File With /c2v To Convert It Into Streamable Video File</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_THUMB_FOUND = "No Thumbnail found ☠️"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters 🔻🔻"""
+    ABOUT_ME = """<b><u>About Me:</u></b>
+    
+<b>An Multi purpose Bot Which Can :-</b>
+⚡ Rename Telegram Files 
+⚡ Set Custom Thumbnail
+⚡ Convert Files into Video 
+
+📱 Suggestion & Feedback ➜ <a href="https://telegram.dog/dlaize"><b>Mr. Dlaize</b></a>
+    """
+    HELP_USER = """🚨 Oh! You Need Help! Choose Available Commands below!
+───────────────
+<b>Made with Thunder ⚡ by : @Dlaize</b>
+"""
+    
+    RENAME_HELP = """
+<b><u>Rename Commands</u></b>
+➠ Send me any Telegram File or Media.
+➠ Upload any valid File or Media.
+➠ Reply to that message to '/rename' new name.extension for renaming.
+    """
+    C2V_HELP = """
+<b><u>Convert File Commands</u></b>
+➠ Reply To An File With '/c2v' To Convert It Into Video.
+"""
+    THUMBNAIL_HELP = """
+<b><u>Thumbnail Commands</u></b>
+➠ Send a photo to make it as Custom Thumbnail.
+➠ Send '/delthumb' to Deleting Thumbnail.
+➠ Send '/showthumb' for view Current Thumbnail.
+"""
+CCAPTION_HELP = """Here Are The Available Commands In Custom Caption \n\n\n ▪️<code>/scaption</code> Use This Command To Save Your Custom Caption \n<b>Usage:</b> <code>/scaption your caption text</code> \n\n<b>[You Can Use</b> <code>{filename}</code> <b>For showing new file name in the caption]</b> """
