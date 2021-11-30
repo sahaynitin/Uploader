5b4b464db9d94a4480366dc87fa4e1dfc6abd3d1
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 08:19:54 +0530

    Update get_external_link_1.py

--- plugins/get_external_link_1.py
+++ plugins/get_external_link_1.py
@@ -34,7 +34,7 @@ from database.adduser import AddUser
 from pyrogram import Client as Tellybots
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-@Tellybots.on_message(pyrogram.filters.command(["uptoanonfiles"]))
+@Tellybots.on_message(filters.command(["uptoanonfiles"]))
 async def get_link(bot, update):
     await AddUser(bot, update)
     logger.info(update.from_user)
