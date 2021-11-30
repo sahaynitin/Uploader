a98096f11639b44e0e78d31b845992b58f92559b
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 18:19:42 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -22,7 +22,7 @@ else:
     from config import Config
 # the Strings used for this "thing"
 from translation import Translation
-from plugins.custom_thumbnail import *
+from plugins.thumbnail import *
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from helper_funcs.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
 from hachoir.metadata import extractMetadata
