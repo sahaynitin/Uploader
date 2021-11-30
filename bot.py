4a8e1295b63adf802ba9b0a22e69470b8730971b
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 08:27:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-
+from pyrogram import Client as Tellybots
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
