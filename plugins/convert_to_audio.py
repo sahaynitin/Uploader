7add802266ed7ed3dab0f87f05ed8f2b385b0b74
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 07:31:55 +0530

    Update convert_to_audio.py

--- plugins/convert_to_audio.py
+++ plugins/convert_to_audio.py
@@ -34,13 +34,6 @@ from PIL import Image
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["c2a"]))
 async def convert_to_audio(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
-        return
     if (update.reply_to_message is not None) and (update.reply_to_message.media is not None) :
         download_location = Config.DOWNLOAD_LOCATION + "/"
         ab=await bot.send_message(
