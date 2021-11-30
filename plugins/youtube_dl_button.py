5b56ca187f8489f69adb5d44c8f6345fccfb7e04
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-30 13:54:17 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -29,7 +29,7 @@ from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
 # https://stackoverflow.com/a/37631799/4723940
 from database.access import tellybots
 from PIL import Image
-
+from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from PIL import Image
 
